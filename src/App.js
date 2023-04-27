1c6928901faf66f048912038072d019b45ca46b5
Mustafa-Siddique <66897095+Mustafa-Siddique@users.noreply.github.com>
2023-04-27 12:04:58 +0530

    bootstrap-fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-
-import { Card } from 'react-bootstrap';
-import './App.css';
-import { Backgr } from './components/Backgr';
-import { NavTop } from './components/NavTop';
+import { Card, Col, Container, Row } from "react-bootstrap";
+import "./App.css";
+import { Backgr } from "./components/Backgr";
+import { NavTop } from "./components/NavTop";
 // import { Card } from 'react-bootstrap';
-
 
 function App() {
   return (
-   <>
-      <NavTop/>
-      <Backgr/>
+    <>
+      <NavTop />
+      <Backgr />
+      <Container fluid>
+        <Row
+        >
+          <Col className="bg-info">
+            <br />
+            <br />
+            <br />
+          </Col>
+          <Col className="bg-warning">
+            <br />
+            <br />
+            <br />
+          </Col>
+          <Col className="bg-success">
+            <br />
+            <br />
+            <br />
+          </Col>
+          <Col className="bg-danger">
+            <br />
+            <br />
+            <br />
+          </Col>
+        </Row>
+      </Container>
       {/* <Card/> */}
-   </>  
+    </>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import {  Card  } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
 
 export const Card = () => {
   return (
-  
-        <Card>
-           <Card.body>
-            <Card.title>Digital Rport</Card.title>
-            <Card.text>Get All reports online 24x7 using your phone number or email-id</Card.text>
-            <button variant="primary">Download</button>
-
-           </Card.body>
-        </Card>
-      
-  )
-}
+    <Card>
+      <Card.body>
+        <Card.title>Digital Rport</Card.title>
+        <Card.text>
+          Get All reports online 24x7 using your phone number or email-id
+        </Card.text>
+        <button variant="primary">Download</button>
+      </Card.body>
+    </Card>
+  );
+};
